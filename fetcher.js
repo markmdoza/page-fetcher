044295554b367f9c9565ee1f75132e0be24e204f
Mark Mendoza <marklmdoza@gmail.com>
2023-07-18 17:34:12 -0400

    initiate server

---
+++ fetcher.js
@@ -0,0 +1,6 @@
+const request = require('request'); //request HTTP
+const fs = require('fs'); // use file system
+
+const url = process.argv[2]; // index at which the URL will go on the CLI
+const filePath = process.argv[3]; // index at which the './html' will go on the CLI
+
